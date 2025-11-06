c5f2aecbf39eac28ca701dcef7a4b69144891a3e
Martín Pizarro <martin.pizarrom@estudiante.uc.cl>
2025-11-06 12:38:26 -0300

    upd(main.py): StudentProfile structure

--- api/main.py
+++ api/main.py
@@ -1,20 +1,35 @@
+"""
+    Módulo encargado de las interacciónes de API
+    EndPoints disponibles:
+        /predict:
+        /coach:
+"""
+
 from typing import Iterable
 from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 class StudentProfile(BaseModel):
-    edad: int
-    asistencia: float
-    sexo: str
-    asignatura: str
-    establecimiento: str
-    calificaciones: Iterable[float]
+    """Representation of an individual student profile"""
+
+    age: int
+    attendance: float
+    sex: str
+    subject: str
+    establishment: str
+    grades: Iterable[float]
 
 app.post("/predict")
 def predict_result(profile: StudentProfile):
+    """
+        Returns an estimated risk-score of how close the student is from dropping out
+    """
     pass
 
 app.post("/coach")
 def determine_coach(profile: StudentProfile):
+    """
+        Generates a coaching plan for the student
+    """
     pass
